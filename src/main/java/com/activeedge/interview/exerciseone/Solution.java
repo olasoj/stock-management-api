ea7fa490c04ce4c937673b6fd62844d2aac6ab23
olasoj <olasojiige@gmail.com>
2021-11-25 13:53:22 +0100

    Added Math.abs to solution 1

--- src/main/java/com/activeedge/interview/exerciseone/Solution.java
+++ src/main/java/com/activeedge/interview/exerciseone/Solution.java
@@ -1,25 +1,32 @@
 package com.activeedge.interview.exerciseone;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
         int[] ar = {1, 3, 6, 4, 1, 2};
-        int[] tempAr = {1, 3, 6, 4, 1, 2};
-        int[] sortedArr = sort(tempAr);
+        int[] ar2 = {5, -1, -3};
 
-        logIndexOfSmallestNonOccurringInteger(ar, smallestNonRepeatingInteger(sortedArr, ar.length));
+        getTheIndexOfTheLowestNonrecurringInteger(ar);
+    }
+
+    private static void getTheIndexOfTheLowestNonrecurringInteger(int[] ar) {
+        int[] tempAr = Arrays.copyOf(ar, ar.length);
+
+        logIndexOfSmallestNonOccurringInteger(ar, smallestNonRepeatingInteger(sort(tempAr), ar.length));
     }
 
     private static void logIndexOfSmallestNonOccurringInteger(int[] ar, int smallestNonRepeatingInteger) {
         for (int i = 0; i < ar.length; i++)
-            if (ar[i] == smallestNonRepeatingInteger) System.out.println(i);
+            if (Math.abs(ar[i]) == smallestNonRepeatingInteger) System.out.println(i);
     }
 
     public static int[] sort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             int j = i;
-            int temp = arr[j];
+            int temp = Math.abs(arr[j]);
             while (j > 0 && temp < arr[j - 1]) {
-                arr[j] = arr[j - 1];
+                arr[j] = Math.abs(arr[j - 1]);
                 j--;
             }
             arr[j] = temp;
